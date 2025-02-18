2583b22fff0f95618af1c114764b282d461cf0c7
alfbrynn <aliffebriansyah@gmail.com>
2025-02-18 22:36:18 +0700

    Route

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        return 'selamat datang';
+    }
+
+    public function about(){
+        return 'Muhammad Alif Febriansyah/2341720025';
+    }
+
+    public function articles($id){
+        return 'Halaman artikel dengan id'.$id;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,81 +1,97 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+ 
 Route::get('/hello', function () {
  return 'Hello World';
 });
 
-Route::get('/', function () {
-    return 'selamat datang';
+Route::get('/world', function () {
+    return 'World';
 });
 
+// Route::get('/', function (){
+//     return 'selamat datang';
+// });
+
 Route::get('/about', function () {
-    return 'Muhammad Alif Febriansyah/2341720025';
+ return 'Muhammad Alif Febriansyah/2341720025';
 });
 
 // route parameters
 
-Route::get('/user/{name}', function ($name) {
-    return 'Nama saya '.$name;
-});
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya '.$name;
+// });
 
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
 });
 
+Route::get('articles/{id}', function ($id){
+    return 'Halaman artikel dengan ID '.$id;
+}); 
+
 // optional parameters
 
-Route::get('/user/{name?}', function ($name=null) {
-    return 'Nama saya'.$nama;
-});
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama saya'.$name;
+// });
 
-Route::get('/user/{name?}', function ($name='John') {
-return 'Nama saya '.$name;
-});
+// Route::get('/user/{name?}', function ($name='John') {
+// return 'Nama saya '.$name;
+// });
 
 // route name
 
 Route::get('/user/profile', function() {
- //
+    return 'nama saya alif';
 })->name('profile');
+
+// Route::get('/hello', [PageController::class,'hello']);
+
+// Route::get('/', [PageController::class,'index']);
+
+// Route::get('articles/{id}', [PageController::class,'articles']);
+
+// Route::get('/about', function () {
+//     return 'Muhammad Alif Febriansyah/2341720025';
+// });
 
 // route group and routes prefixes
 
-Route::middleware(['first', 'second'])->group(function () {
-    Route::get('/', function () {
-        // Uses first & second middleware...
-    });
-Route::get('/user/profile', function () {
-        // Uses first & second middleware...
-    });
-});
-Route::domain('{account}.example.com')->group(function () {
-    Route::get('user/{id}', function ($account, $id) {
-        //
-    });
-});
-Route::middleware('auth')->group(function () {
-    Route::get('/user', [UserController::class, 'index']);
-    Route::get('/post', [PostController::class, 'index']);
-    Route::get('/event', [EventController::class, 'index']);
-});
+// Route::middleware(['first', 'second'])->group(function () {
+//     Route::get('/', function () {
+//         // Uses first & second middleware...
+//     });
+// Route::get('/user/profile', function () {
+//         // Uses first & second middleware...
+//     });
+// });
+// Route::domain('{account}.example.com')->group(function () {
+//     Route::get('user/{id}', function ($account, $id) {
+//         //
+//     });
+// });
+// Route::middleware('auth')->group(function () {
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event', [EventController::class, 'index']);
+// });
 
-Route::prefix('admin')->group(function () {
-    Route::get('/user', [UserController::class, 'index']);
-    Route::get('/post', [PostController::class, 'index']);
-    Route::get('/event', [EventController::class, 'index']);
-});
+// Route::prefix('admin')->group(function () {
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event', [EventController::class, 'index']);
+// });
 
-Route::redirect('/here', '/there');
+// Route::redirect('/here', '/there');
 
-// view routes
+// // view routes
 
-Route::view('/welcome', 'welcome');
-Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+// Route::view('/welcome', 'welcome');
+// Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
 
 
